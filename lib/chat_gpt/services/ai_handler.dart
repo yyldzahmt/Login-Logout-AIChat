544db56af5019279ccc735abce2603c3bbe5477e
Ahmet Yıldız <65078010+yyldzahmt@users.noreply.github.com>
2023-07-13 18:58:28 +0300

    Update ai_handler.dart

--- lib/chat_gpt/services/ai_handler.dart
+++ lib/chat_gpt/services/ai_handler.dart
@@ -2,7 +2,7 @@ import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class AIHandler {
   final _openAI = OpenAI.instance.build(
-    token: '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',
+    token: '<Api token>',
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
